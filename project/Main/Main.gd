54c3a281aa57c51bf6c23d13eed95daf8b9e1f8f
the-alex-g <alex.gestwicki@gmail.com>
2022-04-03 07:11:35 -0400

    Move ring generation and management to new script

--- project/Main/Main.gd
+++ project/Main/Main.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 const PLANT_TIME_STEP := 1.1
 
-var _1_spaces := []
-var _2_spaces := []
-var _3_spaces := []
-var _4_spaces := []
+var _1_spaces := RingDictionary.new()
+var _2_spaces := RingDictionary.new()
+var _3_spaces := RingDictionary.new()
+var _4_spaces := RingDictionary.new()
 var _plant_time := 2.0
 var _game_over := false
 
@@ -16,7 +16,8 @@ onready var _plant_timer = $PlantAdvanceTimer as Timer
 func _ready()->void:
 	randomize()
 	for x in 4: # 0-3
-		set("_" + str(x + 1) + "_spaces", _generate_spaces(9 - x * 2, x))
+		var list_name := "_" + str(x + 1) + "_spaces"
+		get(list_name).generate(9 - x * 2, x)
 
 
 func _generate_spaces(side_length:int, offset:int)->Array:
@@ -51,17 +52,16 @@ func _add_plant()->void:
 	var running := true
 	while running:
 		var ring_name := "_" + str(ring_index) + "_spaces"
-		var ring = get(ring_name) as Array
 	
-		if ring.size() > 0:
-			var plant_position = ring[randi() % ring.size()] as Vector2
+		if get(ring_name).is_not_full():
+			var empty_positions = get(ring_name).get_empty_keys() as Array
+			print(empty_positions.size())
+			var plant_position = empty_positions[randi() % empty_positions.size()] as Vector2
 		
 			while _tilemap.get_cellv(plant_position) != -1:
-				plant_position = ring[randi() % ring.size()]
+				plant_position = empty_positions[randi() % empty_positions.size()]
 			
-			ring.erase(plant_position)
-			set(ring_name, ring)
-			print(get(ring_name).size())
+			get(ring_name).fill(plant_position)
 			
 			_tilemap.set_cellv(plant_position, 0)
 			
@@ -70,7 +70,7 @@ func _add_plant()->void:
 		else:
 			if ring_index < 4:
 				ring_index += 1
-				print("proceed")
 			else:
 				_game_over = true
 				running = false
+				print("over")
