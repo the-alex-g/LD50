b34befabef0d5f68b968b7c9b62742e74c48769c
the-alex-g <alex.gestwicki@gmail.com>
2022-04-03 11:49:12 -0400

    Change vial to bottle and add tip animation

--- project/Mouse/CursorManager.gd
+++ project/Mouse/CursorManager.gd
@@ -15,8 +15,14 @@ func _process(_delta:float)->void:
 
 func empty()->void:
 	_mouse_follower.frame = 0
-	_mouse_follower.play("Filling")
+	_mouse_follower.play("Dumping")
 
 
 func update_anim_time(new_anim_time:float)->void:
+	new_anim_time -= 0.6
 	_mouse_follower.frames.set_animation_speed("Filling", 15.0 / new_anim_time)
+
+
+func _on_AnimatedSprite_animation_finished()->void:
+	if _mouse_follower.animation == "Dumping":
+		_mouse_follower.play("Filling")
