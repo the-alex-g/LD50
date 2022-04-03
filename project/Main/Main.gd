6d956ee2d5019e71d8f685ce7fd67a7c88dc4a79
the-alex-g <alex.gestwicki@gmail.com>
2022-04-03 07:16:12 -0400

    Fix endgame issues

--- project/Main/Main.gd
+++ project/Main/Main.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 const PLANT_TIME_STEP := 1.1
 
+export var _plant_time := 1.0
+
 var _1_spaces := RingDictionary.new()
 var _2_spaces := RingDictionary.new()
 var _3_spaces := RingDictionary.new()
 var _4_spaces := RingDictionary.new()
-var _plant_time := 2.0
 var _game_over := false
 var _loaded := true
 
@@ -51,15 +52,12 @@ func _add_plant()->void:
 	var running := true
 	while running:
 		var ring_name := "_" + str(ring_index) + "_spaces"
+		
 	
 		if get(ring_name).is_not_full():
 			var empty_positions = get(ring_name).get_empty_keys() as Array
 			print(empty_positions.size())
 			var plant_position = empty_positions[randi() % empty_positions.size()] as Vector2
-		
-			while _tilemap.get_cellv(plant_position) != -1:
-				plant_position = empty_positions[randi() % empty_positions.size()]
-			
 			get(ring_name).fill(plant_position)
 			
 			_tilemap.set_cellv(plant_position, 0)
@@ -69,7 +67,7 @@ func _add_plant()->void:
 		else:
 			if ring_index < 4:
 				ring_index += 1
-			else:
+			elif ring_index == 4:
 				_game_over = true
 				running = false
 				print("over")
