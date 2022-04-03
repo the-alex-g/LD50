2c8efeffed2d442f94ffc96358040f49eaf81ba3
the-alex-g <alex.gestwicki@gmail.com>
2022-04-03 16:26:37 -0400

    Incorporate music and add audio buses

--- project/Main/CustomClasses/SoundCheckBox.gd
+++ project/Main/CustomClasses/SoundCheckBox.gd
@@ -2,10 +2,12 @@ extends CheckBox
 
 signal clicked(is_pressed)
 
+var _pressed := false
 
 func _on_Checkbox_toggled(button_pressed:bool)->void:
 	$AudioStreamPlayer.play()
+	_pressed = button_pressed
 
 
 func _on_AudioStreamPlayer_finished()->void:
-	emit_signal("clicked")
+	emit_signal("clicked", _pressed)
